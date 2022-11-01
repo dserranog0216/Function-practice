4a68e466966ae28f5e6d2226f5fc495ed1529cb9
David Serrano <113551943+dserranog0216@users.noreply.github.com>
2022-11-01 20:18:53 +0000

    returns

--- function.py
+++ function.py
@@ -23,7 +23,7 @@ def welcome(name):
 ########################################################################################################################
 # Create Functions Practice #3
 # Declare a function called square, that takes any number as an argument, and each time it is called, it prints the square of that number on the screen (that is, the value to the second power).
-
+import math 
 def square(num, power):
   print(num **power) 
 # The name of the argument that this function must take is number. Create this variable and assign it any number.
